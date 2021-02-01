c8844a5c9e6340fb2d080bcfd5f0c9bd07d65e7f
nahuelba <70066008+nahuelba@users.noreply.github.com>
2021-02-01 18:42:08 -0300

    Delete env directory

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\nahue\dropbox\proyectos\encuentralibre-apiforms\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
